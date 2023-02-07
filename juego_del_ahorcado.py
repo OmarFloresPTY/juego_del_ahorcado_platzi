10b5cda213e9c1a4f3e93f47f5d0e7c845ac8b31
Omar Flores <oafo2699@gmail.com>
2023-02-07 16:51:24 -0500

    Mejoramos el home, eliminamos variables basuras, mejoramos la lógica de programación

--- juego_del_ahorcado.py
+++ juego_del_ahorcado.py
@@ -29,96 +29,88 @@ def read_title():
     except OSError:
         print("No se encontró el archivo title.txt")
 
-def life(life_score):
+def life(life_score, palabra):
     try:
         if life_score == 5:
             with open("./frames/ahorcado-1.txt","r",encoding="utf-8")as f:
                 for hangman in f:
                     print(hangman)
+            return True
         elif life_score == 4:
             with open("./frames/ahorcado-2.txt","r",encoding="utf-8")as f:
                 for hangman in f:
                     print(hangman)
+            return True
         elif life_score == 3:
             with open("./frames/ahorcado-3.txt","r",encoding="utf-8")as f:
                 for hangman in f:
                     print(hangman)
+            return True
         elif life_score == 2:
             with open("./frames/ahorcado-4.txt","r",encoding="utf-8")as f:
                 for hangman in f:
                     print(hangman)
+            return True
         elif life_score == 1:
             with open("./frames/ahorcado-5.txt","r",encoding="utf-8")as f:
                 for hangman in f:
                     print(hangman)
+            return True
         elif life_score == 0:
             with open("./frames/ahorcado-6.txt","r",encoding="utf-8")as f:
                 for hangman in f:
                     print(hangman)
+            print(f"Perdiste, has fallado 5 intentos T.T, la palabra era: {palabra}")
+            return False
     except OSError:
         print("No se encuentran los archivos en la carpeta frame")
 
 def run():
     read_title()
     os.system("Pause")
+    os.system("clear")
     words_create()
     choose = random.randint(1,6)
     palabra = read_words(choose)
-
     list_guess = [" _ " for i in range(1, len(palabra))]
     ban = True
     y = 0
-    z = 0
     life_score = 5
-    os.system("clear")
-    life(life_score) 
-    print(''.join(list_guess))  
-
     while ban == True:
-        error_input = False
-        conval = False  
+        os.system("clear")
+        life(life_score,palabra) 
+        print(''.join(list_guess))  
         w = input("Introduce una letra: ")
         w = w.upper()
-        
         error_input_lambda = lambda string: string.isnumeric() or len(string) == 0 or len(w) > 1
-        error_input = error_input_lambda(w)
+        error_input_lambda = error_input_lambda(w)
         fun_conval = lambda string: string in palabra
-        conval = fun_conval(w)
-
-        if conval == False:
-            life_score = life_score - 1 
-
-        if error_input == True:
+        fun_conval = fun_conval(w)
+        if error_input_lambda == True:
             print("El juego no acepta cadenas vacias, ni frases, ni numeros. Vuelva a intentarlo.")
+            os.system("Pause")
         else:
-            i = 0
-            
-            os.system("clear") 
-            clear = lambda string: string in list_guess
-            clear = not(clear(w))
-
-            if clear == True:
-                for letra in palabra:
-                    if w == letra:
-                        list_guess[i] = w
-                        y = y + 1
-                    i = i + 1 
-                if y == len(list_guess):
-                    print("GANASTE EL JUEGO DEL AHORCADO, LA PALABRA FUE: ") 
-                    ban = False
-                       
+            if fun_conval == False:
+                life_score = life_score - 1
+                ban = life(life_score, palabra)
             else:
-                print(f"La letra {w} ya fue utilizada, intente otra letra")
-                life_score = life_score - 1 
-            
-            life(life_score) 
-            print("\n",''.join(list_guess))
+                i = 0
+                os.system("clear") 
+                clear = lambda string: string in list_guess
+                clear = not(clear(w))
 
-            if life_score == 0:
-                ban = False
-                os.system("cls")
-                life(life_score)
-                print(f"Perdiste, has fallado 5 intentos T.T, la palabra era: {palabra}")
-
+                if clear == True:
+                    for letra in palabra:
+                        if w == letra:
+                            list_guess[i] = w
+                            y = y + 1
+                        i = i + 1 
+                    if y == len(list_guess):
+                        ban = life(life_score,palabra)
+                        print("GANASTE EL JUEGO DEL AHORCADO, LA PALABRA FUE: ",''.join(list_guess)) 
+                        ban = False
+                else:
+                    life_score = life_score - 1
+                    ban = life(life_score,palabra)
 if __name__ == '__main__':
     run()
