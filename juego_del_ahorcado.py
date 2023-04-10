e2df74c1af2fe88307c97b9fd8b0c8c3bd8f802a
OmarUbuntu <oafo9926@gmail.com>
2023-04-09 23:08:19 -0500

    Agregamos una funcion que pausa y limpia la pantalla sin importar el SO

--- juego_del_ahorcado.py
+++ juego_del_ahorcado.py
@@ -1,5 +1,21 @@
 import os
 import random
+import platform
+
+def cls():
+    if os.name == "posix":
+        os.system ("clear")
+    elif os.name == ("ce", "nt", "dos"):
+        os.system ("cls")
+
+def pause():
+    sistema_operativo = platform.system()
+    if sistema_operativo == "Windows":
+        os.system("pause")
+    elif sistema_operativo == "Linux":
+        input("Presione Enter para continuar...")
+    else:
+        print("Lo siento, no se puede pausar la salida en este sistema operativo.")
 
 def words_create():
     words_game = ["PANAMA","AGUACATE","AHORCADO","PYTHON","PROGRAMACION","CANAL"]
@@ -67,8 +83,8 @@ def life(life_score, palabra):
 
 def run():
     read_title()
-    os.system("Pause")
-    os.system("clear")
+    pause()
+    cls()
     words_create()
     choose = random.randint(1,6)
     palabra = read_words(choose)
@@ -77,7 +93,7 @@ def run():
     y = 0
     life_score = 5
     while ban == True:
-        os.system("clear")
+        cls()
         life(life_score,palabra) 
         print(''.join(list_guess))  
         w = input("Introduce una letra: ")
@@ -88,14 +104,14 @@ def run():
         fun_conval = fun_conval(w)
         if error_input_lambda == True:
             print("El juego no acepta cadenas vacias, ni frases, ni numeros. Vuelva a intentarlo.")
-            os.system("Pause")
+            pause()
         else:
             if fun_conval == False:
                 life_score = life_score - 1
                 ban = life(life_score, palabra)
             else:
                 i = 0
-                os.system("clear") 
+                cls()
                 clear = lambda string: string in list_guess
                 clear = not(clear(w))
 
